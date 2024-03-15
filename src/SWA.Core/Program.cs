55edac7a98ee02f66a76cdaf7cc9c9c096dc6bf4
KalvinVilla <villa.kalvin@gmail.com>
2024-03-15 15:26:04 +0100

    chore: initialize structure

---
+++ src/SWA.Core/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceProcess;
+
+namespace SWA.Core
+{
+    internal static class Program
+    {
+
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new SWAService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
